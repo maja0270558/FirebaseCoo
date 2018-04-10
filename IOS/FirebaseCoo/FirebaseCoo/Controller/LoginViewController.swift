9c7e79b3f6103e5f3d03ff6f85738410502d182a
Jojo Lin <maja0270558@gmail.com>
2018-04-10 10:48:45 +0800

    Create user.

---
+++ IOS/FirebaseCoo/FirebaseCoo/Controller/LoginViewController.swift
@@ -0,0 +1,43 @@
+//
+//  LoginViewController.swift
+//  FirebaseCoo
+//
+//  Created by 大容 林 on 2018/4/10.
+//  Copyright © 2018年 DjangoCode. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var emailTextfield: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func pressLogin(_ sender: UIButton) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
